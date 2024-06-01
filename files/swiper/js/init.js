a4a446ecb932afd564f58c4255347cc05b8d4a75
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-06-01 10:52:11 +0300

    Add files via upload

--- files/swiper/js/init.js
+++ files/swiper/js/init.js
@@ -31,3 +31,41 @@ if (partnersSlider) {
       }
    });
 }
+
+const gallerySlider = document.querySelector('.gallery__slider')
+if (gallerySlider) {
+   new Swiper(gallerySlider, {
+      navigation: {
+         prevEl: '#gallery-slider-button-prev',
+         nextEl: '#gallery-slider-button-next',
+      },
+      slidesPerView: 4,
+      watchOverflow: true,
+      spaceBetween: 22,
+      //loop: true,
+      speed: 800,
+      effect: 'slide',
+      pagination: {
+         el: '#gallery-slider-pagination',
+         clickable: true,
+      },
+      breakpoints: {
+         0: {
+            slidesPerView: 2,
+            spaceBetween: 11,
+         },
+         480: {
+            slidesPerView: 2,
+            spaceBetween: 11,
+         },
+         721: {
+            slidesPerView: 3,
+            spaceBetween: 12,
+         },
+         1025: {
+            slidesPerView: 4,
+            spaceBetween: 22,
+         }
+      }
+   });
+}
