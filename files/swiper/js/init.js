883ad6b382a5a4657dea7ad13f9874e1edd48293
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-06-14 15:51:52 +0300

    Add files via upload

--- files/swiper/js/init.js
+++ files/swiper/js/init.js
@@ -1,3 +1,16 @@
+const lazyImages = document.querySelectorAll('img[loading="lazy"]'); // Получаем все изображения с атрибутом loading="lazy"
+function addLoadedClass(image) { // Функция для добавления класса к родителю изображения после его загрузки
+   const parentElement = image.parentElement;
+   if (image.complete) { // Проверяем, загружено ли изображение
+      parentElement.classList.add('loaded');
+   } else {
+      image.addEventListener('load', function() { // Добавляем событие load, чтобы добавить класс после загрузки изображения
+         parentElement.classList.add('loaded');
+      });
+   }
+}
+lazyImages.forEach(addLoadedClass); // Перебираем все изображения и вызываем функцию addLoadedClass для каждого
+
 const partnersSlider = document.querySelector('.partners__slider')
 if (partnersSlider) {
    new Swiper(partnersSlider, {
@@ -70,6 +83,52 @@ if (gallerySlider) {
          1025: {
             slidesPerView: 4,
             spaceBetween: 22,
+         }
+      }
+   });
+}
+
+const pageAboutUsSlider = document.querySelector('.page-about-us__slider')
+if (pageAboutUsSlider) {
+   new Swiper(pageAboutUsSlider, {
+      navigation: {
+         prevEl: '#page-about-us-slider-button-prev',
+         nextEl: '#page-about-us-slider-button-next',
+      },
+      slidesPerView: 3,
+      watchOverflow: true,
+      spaceBetween: 16,
+      //loop: true,
+      speed: 800,
+      effect: 'slide',
+         // Отключить предзагрузка картинок
+      preloadImages: false,
+
+      // Lazy Loading (подгрузка картинок)
+      lazy: {
+         // Подгружать на старте переключения слайда
+         loadOnTransitionStart: false,
+
+         // Подгрузить предыдущую и следующую картинку
+         loadPrewNext: false,
+      },
+      // Слежка за видимыми слайдами
+      watchSlidesProgress: true,
+
+      // Добавление класса видимым слайдам
+      watchSlidesVisibility: true,
+      breakpoints: {
+         0: {
+            slidesPerView: 1.1,
+         },
+         481: {
+            slidesPerView: 2.1,
+         },
+         576: {
+            slidesPerView: 2,
+         },
+         669: {
+            slidesPerView: 3,
          }
       }
    });
